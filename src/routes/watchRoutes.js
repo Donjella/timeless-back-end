ddfc35dc2730249221bb550af523adb3e81d178f
Donjella <brendonang@gmail.com>
2025-03-13 23:50:44 +1100

    feat: update watch routes to include public and admin routes

--- src/routes/watchRoutes.js
+++ src/routes/watchRoutes.js
@@ -7,22 +7,15 @@ const {
   updateWatch, 
   deleteWatch 
 } = require('../controllers/watchController');
-
-// Middleware will be added later for protected routes
-// const { protect, admin } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 
 // Public routes
 router.get('/', getWatches);
 router.get('/:id', getWatchById);
 
-// Protected routes (Admin only) - commented out until auth middleware is implemented
-// router.post('/', protect, admin, createWatch);
-// router.put('/:id', protect, admin, updateWatch);
-// router.delete('/:id', protect, admin, deleteWatch);
-
-// Temporary versions without auth for development
-router.post('/', createWatch);
-router.put('/:id', updateWatch);
-router.delete('/:id', deleteWatch);
+// Protected routes (Admin only)
+router.post('/', protect, admin, createWatch);
+router.put('/:id', protect, admin, updateWatch);
+router.delete('/:id', protect, admin, deleteWatch);
 
 module.exports = router;
